2b39e3e320889de939361b4df668e474a047abf8
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-09-23 11:44:24 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <BrowserRouter>
+        <Toaster position="top-center" />
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
+);
