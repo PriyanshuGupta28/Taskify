61a97e9ed25fe1612739f7919b41d9ed4273a488
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-09-23 11:45:40 +0530

    Update firebase.tsx

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,31 +1,26 @@
-// src/firebase/firebase.ts
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getAnalytics, Analytics } from "firebase/analytics";
+import { getAuth, Auth } from "firebase/auth";
+import { getFirestore, Firestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp, FirebaseApp } from 'firebase/app';
-import { getAnalytics, Analytics } from 'firebase/analytics';
-import { getAuth, Auth } from 'firebase/auth';
-import { getFirestore, Firestore } from 'firebase/firestore';
-
-// Define a TypeScript interface for your Firebase configuration
 interface FirebaseConfig {
-    apiKey: string;
-    authDomain: string;
-    projectId: string;
-    storageBucket: string;
-    messagingSenderId: string;
-    appId: string;
-    measurementId?: string;
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId?: string;
 }
 
-// Your web app's Firebase configuration using environment variables
 const firebaseConfig: FirebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY as string,
-    authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
-    projectId: import.meta.env.VITE_PROJECT_ID as string,
-    storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
-    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
-    appId: import.meta.env.VITE_APP_ID as string,
-    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -36,5 +31,4 @@ const analytics: Analytics = getAnalytics(app);
 const auth: Auth = getAuth(app);
 const db: Firestore = getFirestore(app);
 
-// Export the services for use in other parts of your app
 export { app, analytics, auth, db };
